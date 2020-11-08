cc839cf6918f59c3887fb2ab6e08b763d45e4585
almerindopaixao <almerindo_paixao@live.com>
2020-11-07 21:18:03 -0300

    Update blog-strapi

---
+++ blog-strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3da597831a0f9ed020ff28e90249e2f2'),
+    },
+  },
+});
---
+++ blog-strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c4b58a9c-1250-41a9-819c-c524981796de'
+};
