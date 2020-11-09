eab30e1fd1b8e79423355a57b637509d16d48dc3
almerindopaixao <almerindo_paixao@live.com>
2020-11-08 22:02:37 -0300

    Update getStaticProps

--- blog-next/src/pages/index.tsx
+++ blog-next/src/pages/index.tsx
@@ -1,3 +1,32 @@
-export default function Home() {
-  return <h1>Hello world</h1>;
+import { GetStaticProps } from 'next';
+import { PostData } from '../domain/posts/post';
+
+const getAllPosts = async (): Promise<PostData[]> => {
+  const posts = await fetch('https://blog-strapi-heroku.herokuapp.com/posts');
+
+  const jsonPosts = await posts.json();
+  return jsonPosts;
+};
+
+export type HomeProps = {
+  posts: PostData[];
+};
+
+export default function Home({ posts }: HomeProps) {
+  return (
+    <div>
+      {posts.map((post) => (
+        <h2 key={post.slug}>{post.title}</h2>
+      ))}
+    </div>
+  );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = await getAllPosts();
+
+  return {
+    props: { posts },
+    revalidate: 10,
+  };
+};
